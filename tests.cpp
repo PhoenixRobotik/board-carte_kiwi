ab3b328189d3fd5f2aa3cd03a8114dd371710ff1
larchuto <larchuto@users.noreply.github.com>
2017-05-03 23:01:32 +0200

    tests with two motors

--- tests.cpp
+++ tests.cpp
@@ -34,8 +34,8 @@ public:
             Pin(PortA, Pin::p1),
             Pin(PortA, Pin::p2),
             AltFunction::f1)
-    , motor_right(pwm_Mot1, hallsensor1)
-    , motor_left(pwm_Mot2, hallsensor2)
+    , motor_right(pwm_Mot2, hallsensor2)
+    , motor_left(pwm_Mot1, hallsensor1, true)
     // , usart1(USART1,
     //         Pin(PortB, Pin::p7),
     //         Pin(PortB, Pin::p6))
@@ -115,6 +115,7 @@ int main(int argc, char const *argv[]) {
     int percent = 0;
     int step = 5;
     kiwi->motor_right.enable();
+    kiwi->motor_left.enable();
     kiwi->sleep_ms(200);
 
     std::vector<uint8_t> data;
@@ -122,17 +123,18 @@ int main(int argc, char const *argv[]) {
     bool ledsOn = true;
     int i = 0;
     while(true) {
-        kiwi->motor_right.set_percent_speed(percent);
+        kiwi->motor_right.set_percent_speed(20);
+        kiwi->motor_left.set_percent_speed(20);
         // kiwi.motor_right.set_rot_per_min_speed(percent*1180/100);
         // kiwi.motor_right.set_rot_per_sec_speed(percent*1180/100/60);
         // kiwi->activeLed.set(eepromStatus ? true : ledsOn);
 
         data = std::vector<uint8_t>(4, i++);
 
-        uint32_t rotation_speed_wheel1 = 1000 * kiwi->motor_right.get_rot_per_sec_speed();
-        // uint32_t rotation_speed_wheel2 = 1000 * kiwi->motor_left.get_rot_per_sec_speed();
+        uint32_t rotation_speed_wheel1 = 1000 * 1000/60/kiwi->hallsensor1.get_pulse_period_ms();
+        uint32_t rotation_speed_wheel2 = 1000 * kiwi->motor_left.get_rot_per_sec_speed();
         kiwi->canBus.send(1, rotation_speed_wheel1);
-        // kiwi->canBus.send(2, rotation_speed_wheel2);
+        kiwi->canBus.send(2, rotation_speed_wheel2);
 
         // int32_t distance_wheel1 =
         //     // 75*3.14159/60*
